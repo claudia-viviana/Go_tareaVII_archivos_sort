d8ea2560281e9900dd1de2d0337efcbe0a79cd5d
Claudia De la Cerda <claudia.delacerda7880@alumnos.udg.mx>
2020-10-28 23:06:17 -0600

    Menu integrado

--- main.go
+++ main.go
@@ -9,48 +9,74 @@ import (
 )
 
 func main() {
-	var n int64
-	var str string
+	var opc int64
 	slice := []string{}
-
 	scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Print("Cantidad de cadenas a ingresar: ")
-	fmt.Scanln(&n)
+	for true {
+		fmt.Println("Selecciona una opción: ")
+		fmt.Println("1. Capturar strings")
+		fmt.Println("2. Crear Archivo de strings ordenadas Ascendentemente")
+		fmt.Println("3. Crear Archivo de strings ordenadas Descendentemente")
+		fmt.Println("4. Salir")
 
-	for i := 0; i < int(n); i++ {
-		scanner.Scan()
-		str = scanner.Text()
-		slice = append(slice, str)
+		fmt.Scanln(&opc)
+
+		switch {
+		// CAPTURAR STRINGS _______________________________________________________________
+		case opc == 1:
+			var n int64
+			var str string
+
+			fmt.Print("Cantidad de strings a ingresar: ")
+			fmt.Scanln(&n)
+
+			for i := 0; i < int(n); i++ {
+				scanner.Scan()
+				str = scanner.Text()
+				slice = append(slice, str)
+			}
+
+		// ASCENDENTE ________________________________________________________________
+		case opc == 2:
+
+			sort.Sort(sort.StringSlice(slice))            // Ordenar slice de strings
+			slice2 := []string{strings.Join(slice, "\n")} // Agregar saltos de linea entre cada string
+
+			file, err := os.Create("ascendente.text")
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer file.Close()
+
+			for _, e := range slice2 {
+				file.WriteString(e)
+			}
+
+		// DESCENDENTE ________________________________________________________________
+		case opc == 3:
+
+			sort.Sort(sort.Reverse(sort.StringSlice(slice))) // Ordenar slice de strings
+			slice3 := []string{strings.Join(slice, "\n")}    // Agregar saltos de linea entre cada string
+
+			file2, err := os.Create("descendente.text")
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer file2.Close()
+
+			for _, e := range slice3 {
+				file2.WriteString(e)
+			}
+
+		// Salir _______________________________________________________________
+		case opc == 4:
+			return
+		}
 	}
 
-	sort.Sort(sort.StringSlice(slice))
-	slice2 := []string{strings.Join(slice, "\n")}
-
-	file, err := os.Create("ascendente.text")
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer file.Close()
-
-	for _, e := range slice2 {
-		file.WriteString(e)
-	}
-
-	sort.Sort(sort.Reverse(sort.StringSlice(slice)))
-	slice3 := []string{strings.Join(slice, "\n")}
-
-	file2, err := os.Create("descendente.text")
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer file2.Close()
-
-	for _, e := range slice3 {
-		file2.WriteString(e)
-	}
 }
